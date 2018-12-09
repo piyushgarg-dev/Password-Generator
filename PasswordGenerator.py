0ea5b3581f5db4f673002d481b4d44a14366d966
Piyush Garg <gargpiyush195@gmail.com>
2018-12-09 10:20:53 +0530

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,6 +3,5 @@ print("Password Generator \n")
 fname=input("Please Enter Your First Name: ")
 lname=input("Please Enter Your Last Name: ")
 mobile=input("Contact No: ")
-9023481563
 Password="@"+fname[0:5]+lname+mobile[-3:]
 print("Your Password --> \t"+Password)
